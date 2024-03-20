6580276c8d3dc1a6ccde122231aae04002b40c8e
Jeanpall <110552270+Jeanpall@users.noreply.github.com>
2024-03-20 15:20:10 -0500

    Update hola_mundo.py

--- hola_mundo.py
+++ hola_mundo.py
@@ -2,7 +2,7 @@ f = open('holamundo.html', 'w')
 
 mensaje = """<html>
 <head></head>
-<body><p>Hola Mundo!</p></body>
+<body><p>Hola Mundo, FICHA: 2558346!</p></body>
 </html>"""
 
 f.write(mensaje)
